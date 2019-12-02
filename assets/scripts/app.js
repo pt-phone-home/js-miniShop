608573d1c180c341a2ccc53f952a4135d19ba30f
Peter Tiernan <ptiernan@gmail.com>
2019-12-02 21:59:12 +0000

    updates

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,15 +17,16 @@ class ProductItem {
     prodEl.className = "product-item";
     prodEl.innerHTML = `
         <div>
-            <img src="${prod.imageUrl}" alt="${prod.title}">
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
             <div class="product-item__content">
-                <h2>${prod.title}</h2>
-                <h3>\$${prod.price} </h3>
-                <p>${prod.description} </p>
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price} </h3>
+                <p>${this.product.description} </p>
                 <button> Add to Cart</button>
             </div>
         </div>
       `;
+    return prodEl;
   }
 }
 
@@ -51,6 +52,8 @@ class ProductList {
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
